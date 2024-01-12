80188c35f2cf2632aaf475ce495fc0346c4e7af3
SmEliasdls <111621810+SmEliasdls@users.noreply.github.com>
2024-01-12 20:49:49 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function myFunction(){
+    document.getElementById("p-twxt").innerHTML = 'lorem34';
+  }
+  let carName = "Volvo", person = "Elias", price = "76 million US dollars", cash = 76000000000;
+  document.getElementById("priceDemo").innerHTML = cash;
+
+  let result = myFunction(7,4);
+
+  
+
+  function myFunction(p1, p2) {
+    return p1 + p2;
+  }
+  
+  document.getElementById("functiond").innerHTML = "S M Elias React Concept";
+  document.getElementById("functiondd").innerHTML = result;
+  
